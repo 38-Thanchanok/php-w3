aae4b9a6a6ee1c7224c7b921d49e068cc22b24a4
38-Thanchanok <140575348+38-Thanchanok@users.noreply.github.com>
2024-07-10 09:27:18 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>38-Thanchanok</title>
+</head>
+<body>
+    <?php
+        $a = 3;
+        $b = 4;
+        $c = $a+$b;
+        echo "<h2>ผลลัพธ์</h2>";
+        echo "ค่าของ \$a = 3<br> ค่าของ \$b = 4";
+        echo "<br>";
+        echo "ผลรวมของ \$a และ \$b คือ ".$c."<br>";
+        $A = 20.5;
+        $B = 20.5;
+        $C = $A==$B;
+        echo "<h2>ผลลัพธ์</h2>";
+        echo "\$A = B<br>";
+        echo "\$A = 20.5<br>";
+        echo "\$B = 20.5<br>";
+        echo "ค่า = ".$C;
+    ?>
+</body>
+</html>
